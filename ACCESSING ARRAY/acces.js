a6706144eb3aae2c74ee3e450c37d7e195051016
wfortj <trebrodrigo@gmail.com>
2023-04-07 11:40:05 +0800

    add concat

---
+++ ACCESSING ARRAY/acces.js
@@ -0,0 +1,6 @@
+const lines = ["straight", "diagonal", "borken"];
+
+lines[2] = "broken";
+console.log(lines);
+
+console.log(lines.length);
---
+++ CONCAT ARRAYS/concat.js
@@ -0,0 +1,13 @@
+const boys = ["john", "bert", "steve"];
+const girls = ["anne", "yunah", "annika"];
+
+const attendance = boys.concat(girls);
+
+console.log(attendance.flat());
+
+const shows = [
+  ["asap", "wowowee", "showtime"],
+  ["breaking bad", "who killed sara", "gigolo"],
+];
+
+console.log(shows.flat());
